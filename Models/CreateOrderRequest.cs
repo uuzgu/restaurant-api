d9985a19384ef51955384d1f2e349e332c10dc31
game.dev.bann <bann@gamedevbann-MBP.home>
2025-06-01 19:02:54 +0300

    fix: remove unused AddressRequest class

--- Models/CreateOrderRequest.cs
+++ Models/CreateOrderRequest.cs
@@ -98,28 +98,4 @@ namespace RestaurantApi.Models
         [JsonPropertyName("bell")]
         public string? Bell { get; set; }
     }
-
-    public class AddressRequest
-    {
-        [JsonPropertyName("postalCode")]
-        public string? PostalCode { get; set; }
-
-        [JsonPropertyName("street")]
-        public string? Street { get; set; }
-
-        [JsonPropertyName("house")]
-        public string? House { get; set; }
-
-        [JsonPropertyName("stairs")]
-        public string? Stairs { get; set; }
-
-        [JsonPropertyName("stick")]
-        public string? Stick { get; set; }
-
-        [JsonPropertyName("door")]
-        public string? Door { get; set; }
-
-        [JsonPropertyName("bell")]
-        public string? Bell { get; set; }
-    }
 } 
